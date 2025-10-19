f34978f5f99b63883ff144ee491376936190fd1c
rafaapcode <rafaap2003@gmail.com>
2025-10-19 20:24:05 -0300

    feat: creating the sidebar component

--- src/view/layout/Layout.tsx
+++ src/view/layout/Layout.tsx
@@ -1,8 +1,8 @@
-import { Sidebar } from "lucide-react";
 import { Outlet } from "react-router-dom";
 import { UserRole } from "../../app/entities/User";
 import { useAuth } from "../../app/hooks/useAuth";
 import Unpermissionless from "../../components/Unpermissionless";
+import Sidebar from "./components/sidebar";
 // import NotFoundOrgPage from "../pages/org/notFoundOrg/NotFoundOrg";
 // import NotFoundOrg from "@/view/pages/org/notFoundOrg/NotFoundOrg";
 // import NotFoundOrg from "../components/NotFoundOrg";
