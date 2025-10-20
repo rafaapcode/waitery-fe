68b6c1d08df808a5259d5ff60c70b38656a2c41b
rafaapcode <rafaap2003@gmail.com>
2025-10-19 21:16:40 -0300

    feat: creating the sidebar layout

--- src/view/layout/components/sidebar.tsx
+++ src/view/layout/components/sidebar.tsx
@@ -1,7 +1,13 @@
+import { SidebarIcon } from "lucide-react"
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="bg-red-300 w-16 flex flex-col items-center pt-2">
+      <button><SidebarIcon size={18}/></button>
+      <div className="bg-blue-300 h-full w-full p-2 flex flex-col items-start">
+        <p>teste</p>
+      </div>
+    </div>
   )
 }
 
