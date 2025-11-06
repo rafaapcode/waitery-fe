9c0faa9c6ec457d98046db230af80987182196fc
Rafael Aparecido <rafaap2003@gmail.com>
2025-11-05 21:36:26 -0300

    feat: creating the modal to show details of a order

--- src/view/page/home/components/Column.tsx
+++ src/view/page/home/components/Column.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+import OrderDetailModal from "../../../../components/OrderDetailModal";
 import ColumnItem from "./ColumnItem";
 
 interface ColumnProps {
   icon: string;
   name: string;
-  orders: {table: string; itens: string[]}[];
+  orders: {table: string; status: string; itens: string[]}[];
 }
 
 function Column({ icon, name, orders }: ColumnProps) {
+  const [order, setSelectOrder] = useState<{table: string; status: string; itens: string[]} | null>(null);
+
+  const handleSelectOrder = (selectedOrder: {table: string; status: string; itens: string[]}) => {
+    setSelectOrder(selectedOrder);
+  };
+
   return (
     <div className="flex flex-col p-4 border border-gray-300 bg-gray-50 rounded-lg flex-1 h-full">
+      <OrderDetailModal open={order !== null} onClose={() => setSelectOrder(null)} order={order}/>
       <div className="flex gap-4 items-center justify-center">
         <p>{icon} {name}</p>
         <div className="bg-gray-200 rounded w-fit px-1.5">{orders.length}</div>
@@ -16,7 +25,7 @@ function Column({ icon, name, orders }: ColumnProps) {
       <div className="flex flex-col gap-1 overflow-y-auto max-h-full mt-4">
         {
           orders.map((order, index) => (
-            <ColumnItem key={index} order={order} />
+            <ColumnItem key={index} order={order} onSelectOrder={handleSelectOrder}/>
           ))
         }
       </div>
--- src/view/page/home/components/ColumnItem.tsx
+++ src/view/page/home/components/ColumnItem.tsx
@@ -1,11 +1,12 @@
 
 interface ColumnItemProps {
-  order: {table: string; itens: string[]};
+  order: {table: string; status: string; itens: string[]};
+  onSelectOrder: (order: {table: string; status: string; itens: string[]}) => void;
 }
 
-function ColumnItem({ order }: ColumnItemProps) {
+function ColumnItem({ order, onSelectOrder }: ColumnItemProps) {
   return (
-    <button className="w-full flex flex-col justify-center items-center min-h-14 h-28 bg-gray-100 border border-gray-200 rounded-md">
+    <button onClick={() => onSelectOrder(order)} className="w-full flex flex-col justify-center items-center min-h-14 h-28 bg-white border border-gray-200 rounded-md">
       <p>{order.table}</p>
       <p className="text-gray-400 text-sm">{order.itens.length} Itens</p>
     </button>
