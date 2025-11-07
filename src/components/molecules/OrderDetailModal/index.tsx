98802cfdbb91861c969a5a558ed0fbf82f59a93d
Rafael Aparecido <rafaap2003@gmail.com>
2025-11-06 21:23:00 -0300

    feat: creating a new VARIANT of OrderDetailModal

---
+++ src/components/molecules/OrderDetailModal/index.tsx
@@ -0,0 +1,110 @@
+import { Activity } from "react";
+import type { Order } from "../../../app/entities/Order";
+import { formatCurrency } from "../../../app/lib/formatCurrency";
+import Button from "../../atoms/Button";
+
+import { formatDate } from "../../../app/lib/formatDate";
+import Modal, {
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "../../molecules/Modal";
+import OrderItem from "./OrderItem";
+
+interface OrderDetailModalProps {
+  open: boolean;
+  onClose: () => void;
+  columnName: string;
+  order: Order | null;
+  variant?: "ORDER" | "HISTORY";
+}
+
+function OrderDetailModal({
+  open,
+  onClose,
+  order,
+  columnName,
+  variant = "ORDER",
+}: OrderDetailModalProps) {
+  if (!order) {
+    return null;
+  }
+
+  const products = order.products.map((product) => ({
+    name: product.name,
+    quantity: product.quantity,
+    price: product.price,
+    image: product.image_url ?? "",
+  }));
+
+  const nextButtonTitle =
+    columnName === "🕛 Fila de Espera"
+      ? "Iniciar Preparação"
+      : "Concluir Pedido";
+
+  const isOrderVariant = variant === "ORDER";
+
+  return (
+    <Modal open={open}>
+      <ModalHeader title={order.table} onClose={onClose} />
+      <ModalContent>
+        <div className="w-full flex flex-col gap-8 min-w-[416px] max-w-[480px]">
+          <div className="space-y-2">
+            {isOrderVariant ? (
+              <>
+                <p className="text-sm text-gray-500">Status do Pedido</p>
+                <span className="font-bold">{columnName}</span>
+              </>
+            ) : (
+              <>
+                <p className="text-sm text-gray-500">Data do Pedido</p>
+                <span className="font-bold text-gray-600">{formatDate(order.created_at)}</span>
+              </>
+            )}
+          </div>
+          <div className="space-y-4">
+            <p className="text-sm text-gray-500">Itens</p>
+            <div className="space-y-4">
+              {products.map((order) => (
+                <OrderItem order={order} />
+              ))}
+            </div>
+          </div>
+          <div className="w-full flex justify-between">
+            <p className="text-sm text-gray-500">Total</p>
+            <span className="font-bold">
+              {formatCurrency(order.total_price)}
+            </span>
+          </div>
+        </div>
+      </ModalContent>
+
+      <Activity
+        mode={
+          columnName !== "✅ Pronto" && isOrderVariant
+            ? "visible"
+            : "hidden"
+        }
+      >
+        <ModalFooter>
+          <div className="w-full flex  justify-between items-center">
+            <Button variant="secondary" onClick={onClose}>
+              Cancelar Pedido
+            </Button>
+            <Button onClick={() => {}}>{nextButtonTitle}</Button>
+          </div>
+        </ModalFooter>
+      </Activity>
+
+      <Activity mode={isOrderVariant ? "hidden" : "visible"}>
+        <ModalFooter>
+          <Button variant="secondary" onClick={onClose}>
+            Excluir Registro
+          </Button>
+        </ModalFooter>
+      </Activity>
+    </Modal>
+  );
+}
+
+export default OrderDetailModal;
--- src/view/page/home/components/Column.tsx
+++ src/view/page/home/components/Column.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import type { Order } from "../../../../app/entities/Order";
-import OrderDetailModal from "../../../../components/molecules/OrderDetailModal.tsx";
+import OrderDetailModal from "../../../../components/molecules/OrderDetailModal";
 import ColumnItem from "./ColumnItem";
 
 interface ColumnProps {
@@ -18,17 +18,27 @@ function Column({ icon, name, orders }: ColumnProps) {
 
   return (
     <div className="flex flex-col p-4 border border-gray-300 bg-gray-50 rounded-lg flex-1 h-full">
-      <OrderDetailModal columnName={`${icon} ${name}`} open={order !== null} onClose={() => setSelectOrder(null)} order={order}/>
+      <OrderDetailModal
+        columnName={`${icon} ${name}`}
+        open={order !== null}
+        onClose={() => setSelectOrder(null)}
+        order={order}
+      />
+
       <div className="flex gap-4 items-center justify-center">
-        <p>{icon} {name}</p>
+        <p>
+          {icon} {name}
+        </p>
         <div className="bg-gray-200 rounded w-fit px-1.5">{orders.length}</div>
       </div>
       <div className="flex flex-col gap-1 overflow-y-auto max-h-full mt-4">
-        {
-          orders.map((order, index) => (
-            <ColumnItem key={index} order={order} onSelectOrder={handleSelectOrder}/>
-          ))
-        }
+        {orders.map((order, index) => (
+          <ColumnItem
+            key={index}
+            order={order}
+            onSelectOrder={handleSelectOrder}
+          />
+        ))}
       </div>
     </div>
   );
