1af9740cf79312b1ec9bde40d9c2e3bc1143a7a9
Rafael Aparecido <rafaap2003@gmail.com>
2025-11-14 14:51:33 -0300

    feat: integrating the useForm with the orgPage

--- src/view/page/org/index.tsx
+++ src/view/page/org/index.tsx
@@ -1,3 +1,5 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
 import Button from "../../../components/atoms/Button";
 import Input from "../../../components/atoms/Input";
 import TextArea from "../../../components/atoms/TextArea";
@@ -6,6 +8,10 @@ import Select, {
   SelectContent,
   SelectTrigger,
 } from "../../../components/molecules/Select";
+import {
+  createOrgFormSchema,
+  type CreateOrgFormData,
+} from "./schemas/CreateOrgSchema";
 
 const openHours = [
   { label: "10:00", value: "10" },
@@ -16,7 +22,7 @@ const openHours = [
   { label: "15:00", value: "15" },
   { label: "16:00", value: "16" },
   { label: "17:00", value: "17" },
-  { label: "18:00", value: "18" }
+  { label: "18:00", value: "18" },
 ];
 
 const closeHours = [
@@ -25,76 +31,126 @@ const closeHours = [
   { label: "21:00", value: "21" },
   { label: "22:00", value: "22" },
   { label: "23:00", value: "23" },
-  { label: "00:00", value: "0" }
-]
+  { label: "00:00", value: "0" },
+];
 
 function Org() {
+  const form = useForm<CreateOrgFormData>({
+    resolver: zodResolver(createOrgFormSchema),
+    defaultValues: {},
+  });
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isSubmitting, isDirty },
+  } = form;
+
+  const onSubmit = handleSubmit(async (data) => {
+    console.log(data);
+  });
+
   return (
     <div className="w-full h-full overflow-hidden">
       <div className="w-1/2 max-h-full mt-2 flex flex-col gap-2 items-center justify-center mx-auto overflow-y-auto">
-        <ImageInput label="" className="h-40" />
+        <Controller
+          control={control}
+          name="image"
+          render={({ field }) => (
+            <ImageInput
+              onChange={field.onChange}
+              url={field.value}
+              className="h-40"
+            />
+          )}
+        />
+
         <div className="w-full flex gap-4">
           <Input
+            {...register("name")}
             placeholder="Nome"
-            name="nome"
             placeholderText="Nome da Organização"
+            error={errors.name?.message}
           />
           <Input
+            {...register("email")}
             type="email"
             placeholder="Email"
-            name="email"
             placeholderText="Email da Organização"
+            error={errors.email?.message}
           />
         </div>
 
         <div className="w-full">
-          <TextArea placeholder="Descrição" />
+          <TextArea placeholder="Descrição" {...register("description")} />
         </div>
 
         <div className="w-full flex gap-4">
           <Input
+            {...register("location_code")}
             type="number"
             placeholder="Número"
-            name="numero"
             placeholderText="1298"
+            error={errors.location_code?.message}
           />
           <Input
+            {...register("cep")}
             placeholder="CEP"
-            name="cep"
             placeholderText="CEP da Organização"
+            error={errors.cep?.message}
           />
         </div>
         <div className="w-full flex gap-4">
           <div className="flex-1">
-            <label className="text-gray-600 text-sm">
-              Horário de Abertura
-            </label>
-            <Select
-              onValueChange={(e) => {
-                // field.onChange(e);
-              }}
-            >
-              <SelectTrigger placeholder="Selecionar horário de abertura" />
-              <SelectContent options={openHours} className="p-1" />
-            </Select>
+            <label className="text-gray-600 text-sm">Horário de Abertura</label>
+            <Controller
+              control={control}
+              name="open_hour"
+              render={({ field }) => (
+                <Select
+                  value={String(field.value)}
+                  onValueChange={(e) => {
+                    field.onChange(Number(e));
+                  }}
+                >
+                  <SelectTrigger placeholder="Selecionar horário de abertura" />
+                  <SelectContent options={openHours} className="p-1" />
+                </Select>
+              )}
+            />
           </div>
-           <div className="flex-1">
+          <div className="flex-1">
             <label className="text-gray-600 text-sm">
               Horário de Fechamento
             </label>
-            <Select
-              onValueChange={(e) => {
-                // field.onChange(e);
-              }}
-            >
-              <SelectTrigger placeholder="Selecionar horário de fechamento" />
-              <SelectContent options={closeHours} className="p-1" />
-            </Select>
+            <Controller
+              control={control}
+              name="close_hour"
+              render={({ field }) => (
+                <Select
+                  value={String(field.value)}
+                  onValueChange={(e) => {
+                    field.onChange(Number(e));
+                  }}
+                >
+                  <SelectTrigger placeholder="Selecionar horário de fechamento" />
+                  <SelectContent options={closeHours} className="p-1" />
+                </Select>
+              )}
+            />
           </div>
         </div>
 
         <div className="w-full flex justify-end items-center">
-          <Button size="sm">Salvar Alterações</Button>
+          <Button
+            disabled={!isDirty || isSubmitting}
+            isLoading={isSubmitting}
+            size="sm"
+            onClick={onSubmit}
+          >
+            Salvar Alterações
+          </Button>
         </div>
       </div>
     </div>
---
+++ src/view/page/org/schemas/createOrgSchema.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const createOrgFormSchema = z.object({
+  image: z.file("A imagem da organização é obrigatória").optional(),
+  name: z.string().min(1, "O nome da organização é obrigatório"),
+  email: z.email("O email da organização é obrigatório"),
+  description: z.string().min(1, "A descrição da organização é obrigatória"),
+  location_code: z.string("O número do estabelecimento é obrigatório").min(0, "O número do estabelecimento deve ser válido"),
+  cep: z.string("O número do CEP é obrigatório").min(0, "O número do CEP deve ser válido"),
+  open_hour: z.number("O horário de abertura é obrigatório").min(10, "O horário de abertura deve ser válido").max(18, "O horário de abertura deve ser válido"),
+  close_hour: z.number("O horário de fechamento é obrigatório").min(0, "O horário de fechamento deve ser válido").max(23, "O horário de fechamento deve ser válido"),
+});
+
+export type CreateOrgFormData = z.infer<typeof createOrgFormSchema>;
