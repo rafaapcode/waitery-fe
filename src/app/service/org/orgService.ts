89a683090b62e189378e455cccef146e81db1ec5
Rafael Aparecido <rafaap2003@gmail.com>
2026-01-13 09:00:18 -0300

    fix: normalizing the cep when create a new org

--- src/app/service/org/orgService.ts
+++ src/app/service/org/orgService.ts
@@ -42,7 +42,7 @@ export class OrgService extends Service {
   static async createOrg(org: OrgService.CreateOrgParams): Promise<void> {
     const { data } = await this.client.postForm(
       "/organizations",
-      org
+      {...org, cep: org.cep.replace(/\D/g, "")}
     );
     console.log(data);
     // return data;
