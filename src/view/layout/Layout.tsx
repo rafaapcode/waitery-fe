cdefcaae8c45a945374bc3db62bb2177eb00d35d
Rafael Aparecido <rafaap2003@gmail.com>
2026-02-19 07:22:07 -0300

    fix: if the user is not allowed , logged out them

--- src/view/layout/Layout.tsx
+++ src/view/layout/Layout.tsx
@@ -1,27 +1,34 @@
+import toast from "react-hot-toast";
 import { Outlet } from "react-router-dom";
 import { UserRole } from "../../app/entities/User";
 import { useAuth } from "../../app/hooks/useAuth";
-import Unpermissionless from "../../components/Unpermissionless";
-import Sidebar from "./components/sidebar";
 import NotFoundOrgPage from "../page/org/notFoundOrg/NotFoundOrg";
-
+import Sidebar from "./components/sidebar";
 
 function Layout() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
-  if(!user) {
+  if (!user) {
     return null;
   }
 
   const role = user.role;
+
+  if (role === UserRole.WAITER || role === UserRole.CLIENT) {
+    signOut();
+    toast.error(
+      "Unauthorized access. Please log in with an appropriate account.",
+    );
+    return null;
+  }
+
   return (
     <main className="bg-[#FAFAFA] w-full flex h-screen">
-        {(role === UserRole.OWNER || role === UserRole.ADMIN) && <Sidebar />}
-        <section className="w-full h-full p-6 overflow-y-auto rounded-tl-4xl rounded-bl-4xl bg-white shadow">
-          {(role === UserRole.WAITER || role === UserRole.CLIENT) && <Unpermissionless />}
-          {!user.org.id && <NotFoundOrgPage />}
-          {user.org.id && <Outlet />}
-        </section>
+      {(role === UserRole.OWNER || role === UserRole.ADMIN) && <Sidebar />}
+      <section className="w-full h-full p-6 overflow-y-auto rounded-tl-4xl rounded-bl-4xl bg-white shadow">
+        {!user.org.id && <NotFoundOrgPage />}
+        {user.org.id && <Outlet />}
+      </section>
     </main>
   );
 }
